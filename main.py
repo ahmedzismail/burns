38d079bcc5a41c954cbc39511a562a27d8dea7ae
ahmedzismail <vordmecomm@gmail.com>
2024-11-26 07:08:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#دالة نسبة الحروق
+
 def burns_percent():
   rule_of_nines={
     "head":9,
@@ -10,22 +10,19 @@ def burns_percent():
   }
   burns_percen=0
   print(""""
-  هذا البرنامج لقياس نسبة الحروق في الجسم حسب قاعدة التسعات
-      Rule of nines
-                تعليمات البرنامج
-  من فضلك ادخل اسم المنطفة المصابة بالحرق باللغة الانجليزية
-                     الراس= head = 9%
-                     الذراع الواحد= arm =9%
-                     الساق الواحدة = leg = 18%
-                     الجذع الامامي= anterior trunk = 18%
-                     الجذع الخلفي= posterior trunk = 18%
-                     الاعضاء التناسلية= genitalia = 1%
-     في حالة مااذا كانت الاصابة في الذراعين  او الرجلين نكتب اسم العصو مرتين
-  في حالة الانتهاء من الادخال قم بكتابة
-  end
-    """)
+  this program to calculate burns surface area using
+               Rule of nines
+          instructions
+
+          head = 9%
+          one arm =9%
+          one leg = 18%
+          anterior trunk = 18%
+          posterior trunk = 18%
+          genitalia = 1%
+      """)
   while True:
-    burn_area=input("""من فضلك  ادخل اسم العضو المصاب 
+    burn_area=input(""" Enter your choice of burn area
     head - arm - leg - anterior trunk - posterior trunk - genitalia - end 
     """).lower()
     if burn_area=="end":
@@ -33,7 +30,6 @@ def burns_percent():
     elif burn_area in rule_of_nines.keys():
       burns_percen+=rule_of_nines[burn_area]
     else:
-         print(" wrong choice try again "+"ادخال خاطىء  اعد المحاولة")
+         print(" wrong choice try again ")
   print(f"the total burns area is { burns_percen}%")
 burns_percent()
-    
